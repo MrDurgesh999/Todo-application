21c5c1878d111e5c3a394998db6320802ec1fccd
Durgesh yadav <166358051+MrDurgesh999@users.noreply.github.com>
2025-01-27 18:03:41 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded",function(){
+    let input=document.querySelector("input");
+    let button=document.querySelector("button");
+    let list=document.querySelector(".list");
+
+    button.addEventListener("click",function(){
+        if(input.value.trim()!==""){
+            let newList =document.createElement("li");
+            let span = document.createElement("span");
+            span.innerText =input.value;
+            newList.appendChild(span);
+            let deleteButton=document.createElement("span");
+            deleteButton.innerHTML=`<i class="fa-solid fa-trash"></i>`;
+            deleteButton.className="delete-Btn";
+            deleteButton.addEventListener("click",function(){
+            list.removeChild(newList);
+        })
+            newList.appendChild(deleteButton)
+            list.appendChild(newList);
+            input.value="";
+
+
+        }
+    })
+})
